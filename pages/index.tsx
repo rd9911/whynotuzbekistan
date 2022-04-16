e97216f2e2726ce70d8e9ffb5b9fb0ad66973a31
rd9911 <azizaziz9911@mail.ru>
2022-04-16 11:20:11 +0300

    Set up the project.

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className='flex justify-center'>Hello World!</div>
+  )
+}
+
+export default Home
